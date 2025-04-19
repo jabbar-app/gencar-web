6303b28053a39c71497cfcbf6cd605df91f294f7
Jab <jabbarpanggabean@gmail.com>
2025-04-19 22:33:11 +0700

    Update Pengumuman Gencar

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+
+<html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default"
+  data-assets-path="{{ asset('assets/') }}" data-template="vertical-menu-template-no-customizer">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+  <title>Future Leaders ID</title>
+  <meta name="description" content="" />
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.png') }}" />
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+    rel="stylesheet" />
+
+  <!-- Icons -->
+  <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/fontawesome.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/tabler-icons.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/flag-icons.css') }}" />
+
+  <!-- Core CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
+
+  <!-- Vendors CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/node-waves/node-waves.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/typeahead-js/typeahead.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/@form-validation/form-validation.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/flatpickr/flatpickr.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}" />
+
+  <!-- Page CSS -->
+  <!-- Page -->
+  <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}" />
+
+  <!-- Helpers -->
+  <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+  <script src="{{ asset('assets/js/config.js') }}"></script>
+</head>
+
+<body>
+  <!-- Content -->
+
+  @yield('content')
+
+  <!-- / Content -->
+
+  <!-- Core JS -->
+  <!-- build:js assets/vendor/js/core.js -->
+  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+    crossorigin="anonymous"></script>
+  <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
+  <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/node-waves/node-waves.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/hammer/hammer.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/i18n/i18n.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/typeahead-js/typeahead.js') }}"></script>
+  <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
+
+  <!-- endbuild -->
+
+  <!-- Vendors JS -->
+  <script src="{{ asset('assets/vendor/libs/@form-validation/popular.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/@form-validation/bootstrap5.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/@form-validation/auto-focus.js') }}"></script>
+
+  <!-- Main JS -->
+  <script src="{{ asset('assets/js/main.js') }}"></script>
+
+  <!-- Page JS -->
+  <script src="{{ asset('assets/js/pages-auth.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/cleavejs/cleave.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/cleavejs/cleave-phone.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/moment/moment.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/flatpickr/flatpickr.js') }}"></script>
+  <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}"></script>
+  <script src="{{ asset('assets/js/form-layouts.js') }}"></script>
+
+  @yield('js')
+</body>
+
+</html>
