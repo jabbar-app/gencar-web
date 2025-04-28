4c3eae19bb23ca957222150d45e8bc5cd4621209
Jab <jabbarpanggabean@gmail.com>
2025-04-28 13:46:50 +0700

    feat: Update wording pengumuman GENCAR 8

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,90 +13,92 @@
   @endphp
   <div class="container-xxl flex-grow-1 container-p-y">
     @include('components.session-message')
-    <div class="row">
-      <div class="col-xl-4 mb-4 col-lg-5 col-12">
-        <div class="card">
-          <div class="d-flex align-items-end row">
-            <div class="col-7">
-              <div class="card-body text-nowrap">
-                <h5 class="card-title mb-0">Welcome! 🎉</h5>
-                <p class="mb-2">{{ Auth::user()->name }}</p>
-                <h5 class="text-danger mb-1">
-                  <i class="menu-icon tf-icons ti ti-clock"></i>Tahap {{ $stage }} / 3
-                </h5>
-                @if ($user->status == 'Belum Diterima')
-                  <div class="btn btn-sm btn-outline-danger mt-2">Selesai</div>
-                @else
-                  <div class="btn btn-sm btn-outline-info mt-2">{{ $user->status }}</div>
-                @endif
-              </div>
-            </div>
-            <div class="col-5 text-center text-sm-left">
-              <div class="card-body pb-0 px-0 px-md-4">
-                <img src="../../assets/img/illustrations/card-advance-sale.png" height="140" alt="view sales" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-8 mb-4 col-lg-7 col-12">
-        <div class="card h-100">
-          <div class="card-header">
-            <div class="d-flex justify-content-between mb-3">
-              <h5 class="card-title mb-0">Timeline</h5>
-            </div>
-          </div>
-          <div class="card-body">
-            <div class="row gy-3">
-              <div class="col-md-4 col-6">
-                <div class="d-flex align-items-center">
-                  <div
-                    class="badge rounded-pill @if ($stage == 1) bg-label-info @else bg-label-warning @endif me-3 p-2">
-                    <i class="ti ti-clock ti-sm"></i>
-                  </div>
-                  <div class="card-info">
-                    <h5 class="mb-0">Seleksi Berkas</h5>
-                    @if ($stage == 1 && $user->status != 'Belum Diterima')
-                      <small>In progress</small>
-                    @else
-                      <small>-</small>
-                    @endif
-                  </div>
+    @if ($status == 'Seleksi Berkas' || $status == 'Seleksi Wawancara')
+      <div class="row">
+        <div class="col-xl-4 mb-4 col-lg-5 col-12">
+          <div class="card">
+            <div class="d-flex align-items-end row">
+              <div class="col-7">
+                <div class="card-body text-nowrap">
+                  <h5 class="card-title mb-0">Welcome! 🎉</h5>
+                  <p class="mb-2">{{ Auth::user()->name }}</p>
+                  <h5 class="text-danger mb-1">
+                    <i class="menu-icon tf-icons ti ti-clock"></i>Tahap {{ $stage }} / 3
+                  </h5>
+                  @if ($user->status == 'Belum Diterima')
+                    <div class="btn btn-sm btn-outline-danger mt-2">Selesai</div>
+                  @else
+                    <div class="btn btn-sm btn-outline-info mt-2">{{ $user->status }}</div>
+                  @endif
                 </div>
               </div>
-              <div class="col-md-4 col-6">
-                <div class="d-flex align-items-center">
-                  <div
-                    class="badge rounded-pill @if ($stage == 2) bg-label-info @else bg-label-warning @endif me-3 p-2">
-                    <i class="ti ti-clock ti-sm"></i>
-                  </div>
-                  <div class="card-info">
-                    <h5 class="mb-0">Wawancara</h5>
-                    @if ($stage == 2 && $user->status != 'Belum Diterima')
-                      <small>In progress</small>
-                    @else
-                      <small>-</small>
-                    @endif
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 col-6">
-                <div class="d-flex align-items-center">
-                  <div
-                    class="badge rounded-pill @if (!empty(Auth::user()->result)) bg-label-info @else bg-label-warning @endif me-3 p-2">
-                    <i class="ti ti-clock ti-sm"></i>
-                  </div>
-                  <div class="card-info">
-                    <h5 class="mb-0">Pengumuman</h5>
-                    <small>{{ Auth::user()->result ?? '-' }}</small>
-                  </div>
+              <div class="col-5 text-center text-sm-left">
+                <div class="card-body pb-0 px-0 px-md-4">
+                  <img src="../../assets/img/illustrations/card-advance-sale.png" height="140" alt="view sales" />
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <div class="col-xl-8 mb-4 col-lg-7 col-12">
+          <div class="card h-100">
+            <div class="card-header">
+              <div class="d-flex justify-content-between mb-3">
+                <h5 class="card-title mb-0">Timeline</h5>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="row gy-3">
+                <div class="col-md-4 col-6">
+                  <div class="d-flex align-items-center">
+                    <div
+                      class="badge rounded-pill @if ($stage == 1) bg-label-info @else bg-label-warning @endif me-3 p-2">
+                      <i class="ti ti-clock ti-sm"></i>
+                    </div>
+                    <div class="card-info">
+                      <h5 class="mb-0">Seleksi Berkas</h5>
+                      @if ($stage == 1 && $user->status != 'Belum Diterima')
+                        <small>In progress</small>
+                      @else
+                        <small>-</small>
+                      @endif
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 col-6">
+                  <div class="d-flex align-items-center">
+                    <div
+                      class="badge rounded-pill @if ($stage == 2) bg-label-info @else bg-label-warning @endif me-3 p-2">
+                      <i class="ti ti-clock ti-sm"></i>
+                    </div>
+                    <div class="card-info">
+                      <h5 class="mb-0">Wawancara</h5>
+                      @if ($stage == 2 && $user->status != 'Belum Diterima')
+                        <small>In progress</small>
+                      @else
+                        <small>-</small>
+                      @endif
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 col-6">
+                  <div class="d-flex align-items-center">
+                    <div
+                      class="badge rounded-pill @if (!empty(Auth::user()->result)) bg-label-info @else bg-label-warning @endif me-3 p-2">
+                      <i class="ti ti-clock ti-sm"></i>
+                    </div>
+                    <div class="card-info">
+                      <h5 class="mb-0">Pengumuman</h5>
+                      <small>{{ Auth::user()->result ?? '-' }}</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    @endif
 
     <div class="row">
       <div class="col-12">
